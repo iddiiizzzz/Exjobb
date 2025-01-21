6d3f29519b897ac1f3ed9ba3091bd28cdb2370a5
Ida Berg <bergid@saga.math.chalmers.se>
2025-01-21 13:40:57 +0100

    ny fil

---
+++ blast.py
@@ -0,0 +1,17 @@
+'''
+Commands:
+- blastn
+- makeblastdb
+
+Files:
+- antibiotic_resistance_genes.fna - FASTA-fil med alla resistensgener, detta är vår input fil
+- genome_filepaths.tsv - Paths till alla relevanta bakteriegenom på servern
+
+(Andra filer):
+- count_matrix.tsv - Abundans av resistensgener i metagenom
+- genome_full_lineage.tsv - Komplett taxonomi för alla bakteriegenomen
+- taxonomy_metagenomes.csv - Taxonomi i de metagenomiska proverna genererade med Kraken2
+
+'''
+
+
