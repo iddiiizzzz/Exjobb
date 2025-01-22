885f7b67b9a69e12aab1202dda64c8125238d558
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-01-22 09:55:24 +0100

    example blast results

--- blast.py
+++ blast.py
@@ -12,17 +12,22 @@ Files:
 - genome_full_lineage.tsv - Komplett taxonomi för alla bakteriegenomen
 - taxonomy_metagenomes.csv - Taxonomi i de metagenomiska proverna genererade med Kraken2
 
+/storage/shared/data_for_master_students/ida_and_ellen
+
 ----------------------------------------------------------------------------------------------
 
 to work in conda: conda activate exjobb_env
 
 skriva om id: seq1, seq2, seq3 etc
 
-makeblastdb -in /storage/shared/data_for_master_students/ida_and_ellen/antibiotic_resistance_genes.fna -dbtype nucl -out /storage/bergid/antibiotic_resistance_db -title "Antibiotic Resistance Genes DB" -parse_seqids
+makeblastdb -in shorter_id.fna -dbtype nucl -out /storage/koningen/antibiotic_resistance_db -title "Antibiotic Resistance Genes DB" -parse_seqids
 
-blastn -query query.fna \
-       -db blast_db \
-       -out results.txt
+blastn -query /storage/koningen/GCA_001141985.1_6259_7_14_genomic.fna -db /storage/koningen/antibiotic_resistance_db -out blast_results_example.txt
 
+example query: 
+       /storage/shared/ncbi_bacteria_assembly/GCA/001/141/985/GCA_001141985.1_6259_7_14/GCA_001141985.1_6259_7_14_genomic.fna.gz
+
+------------------------------------------------------------------------------
+gunzip file.fna.gz --> file.fna
 
 '''
