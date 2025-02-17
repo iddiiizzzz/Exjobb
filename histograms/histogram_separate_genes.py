3bc8cbc08f38ced3a5a1cf9ef608c4ab677737cd
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-02-17 15:32:39 +0100

    nya historgamfiler

--- histograms/histogram_separate_genes.py
+++ histograms/histogram_separate_genes.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 count_matrix = "/storage/koningen/count_matrix.tsv"
-highest_raw_counts = "/storage/koningen/ranked_counts/highest_raw_counts.tsv"
-#lowest_raw_counts = "/storage/koningen/ranked_counts/lowest_raw_counts.tsv"
+#highest_raw_counts = "/storage/koningen/ranked_counts/highest_raw_counts.tsv"
+lowest_raw_counts = "/storage/koningen/ranked_counts/lowest_raw_counts.tsv"
 
 num_top_rows = 10
 max_zero_percentage = 0.5
@@ -25,17 +25,17 @@ with open(count_matrix, "r") as infile:
         # Only include rows with more than 75% non-zero
         if zero_percentage <= max_zero_percentage:
            #max_value = max(numeric_values)  # Find the maximum value in the row
-           max_value = max(numeric_values) # Find the minimum value in the row
-           values.append((identifier, max_value))  # Store as tuple (identifier, max_value)
+           min_value = min(numeric_values) # Find the minimum value in the row
+           values.append((identifier, min_value))  # Store as tuple (identifier, max_value)
 
 # Sort
-values.sort(key=lambda x: x[1], reverse=False)
+values.sort(key=lambda x: x[1], reverse=False) # High to low --> True
 
 # Get the top N identifiers based on the highest individual values
 top_identifiers = {identifier for identifier, _ in values[:num_top_rows]}
 
 # Filter the original file to keep only the rows with identifiers in the selected list
-with open(count_matrix, "r") as infile, open(highest_raw_counts, "w") as outfile:
+with open(count_matrix, "r") as infile, open(lowest_raw_counts, "w") as outfile:
     header = next(infile)  # Read and write the header
     outfile.write(header)
 
@@ -48,17 +48,17 @@ with open(count_matrix, "r") as infile, open(highest_raw_counts, "w") as outfile
 
 
 #### Histogram #####
-df = pd.read_csv(highest_raw_counts, sep="\t", skiprows=1).iloc[:, 1:] 
-all_values = df.values.flatten()
+# df = pd.read_csv(highest_raw_counts, sep="\t", skiprows=1).iloc[:, 1:] 
+# all_values = df.values.flatten()
 
-# Apply transformation
-all_values = np.log(all_values + 1)
+# # Apply transformation
+# all_values = np.log(all_values + 1)
 
-# Plot histogram
-plt.figure(figsize=(8, 5))
-plt.hist(all_values, bins=np.arange(all_values.max() + 2) - 0.5, edgecolor='black')
-plt.xlabel("Log-transformed Count Value")
-plt.ylabel("Frequency")
-plt.title(f"Log-transformed counts for the {num_top_rows} genes with the highest individual values")
-plt.xticks(range(int(all_values.max()) + 1))  # Ensure discrete values on x-axis
-plt.savefig("histograms/bilder/genes_filtered/histogram_genes_highest_individual_50.png")
+# # Plot histogram
+# plt.figure(figsize=(8, 5))
+# plt.hist(all_values, bins=np.arange(all_values.max() + 2) - 0.5, edgecolor='black')
+# plt.xlabel("Log-transformed Count Value")
+# plt.ylabel("Frequency")
+# plt.title(f"Log-transformed counts for the {num_top_rows} genes with the highest individual values")
+# plt.xticks(range(int(all_values.max()) + 1))  # Ensure discrete values on x-axis
+# plt.savefig("histograms/bilder/genes_filtered/histogram_genes_highest_individual_50.png")
