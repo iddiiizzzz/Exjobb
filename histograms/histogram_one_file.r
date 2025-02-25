5b0e03469e439a5fa85270dfca01bbf73727c2d9
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-02-25 10:31:58 +0100

    histogram transform

--- histograms/histogram_one_file.r
+++ histograms/histogram_one_file.r
@@ -15,27 +15,26 @@ library(ggplot2)
 
 
 # # Organism counts
-data <- read.table("/storage/koningen/ranked_counts/sum_counts/lowest_sum_counts_org_ww2.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+data <- read.table("/storage/koningen/ranked_counts/sum_counts/lowest_sum_counts_org_hg.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 
 
 values <- as.numeric(unlist(data[, -1]))  # Remove the first column with sample names and convert the rest to numeric
 
 
-log_values <- log(values + 1)# Apply ln transformation
-df <- data.frame(values = log_values)
+values <- log(values+1) # Transformation
+df <- data.frame(values = values)
 
 
 # change binwidth depending on highest count. 1000 for 44 000 000. 1 for 1000(?)
-df <- data.frame(values = values)
 ggplot(df, aes(x = values)) +
   geom_histogram(binwidth = 1, fill = "blue", color = "black", boundary = 0.5) +
-  labs(title = "Histogram of Count Values", x = "Count Value", y = "Number of counts") +
-  theme_bw() +  # Use theme_bw() to set white background
-  ylim(0, 60)
+  labs(title = "Histogram of Log-transformed Count Values", x = "Count Value", y = "Number of counts") +
+  theme_bw()  # Use theme_bw() to set white background
+  #ylim(0, 150)
 
 # Save the plot with white background
-ggsave("histograms/bilder/organisms/organisms_sum_counts/transformed/lowest_sum_ww2_transformed.jpg", bg = "white")
+ggsave("histograms/bilder/organisms/organisms_sum_counts/transformed/lowest_sum_hg_transformed.jpg", bg = "white")
 
 
 # Rscript histograms/histogram_one_file.r
