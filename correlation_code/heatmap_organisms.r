f81319822000db13c118091c99b8cd50d0ec2d1a
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-02-26 10:22:27 +0100

    changed r-script

--- correlation_code/heatmap_organisms.r
+++ correlation_code/heatmap_organisms.r
@@ -10,14 +10,14 @@ library(gplots)
 library(RColorBrewer)
 
 
-correlations <- read.table("test_files/results_org_correlation_ww1_test.tsv", 
-                           sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
+# correlations <- read.table("test_files/results_org_correlation_ww1_test.tsv", 
+#                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
 
-# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww1_r.tsv", 
+# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww1_log.tsv", 
 #                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww2.tsv", 
-#                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_hg.tsv", 
+correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww2_log.tsv", 
+                           sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
+# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_hg_log.tsv", 
 #                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
 
 
@@ -36,8 +36,8 @@ cor_matrix[is.na(cor_matrix)] <- 0
 # Create a color palette using RColorBrewer
 my_palette <- colorRampPalette(brewer.pal(9, "YlOrRd"))(100)
 
-pdf("correlation_code/heatmap_org_ww1.pdf")
-heatmap.2(cor_matrix, main = "Organism Correlation Heatmap", 
+pdf("correlation_code/heatmaps/heatmap_org_ww2.pdf")
+heatmap.2(cor_matrix, main = "WasteWater2 Correlation Heatmap (log-transformed)", 
 col=my_palette,
 trace = "none",
 margins = c(12,12))
