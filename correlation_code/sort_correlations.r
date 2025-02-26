897399b480a5cb37f602d2a62063411cccf33bcd
Iddiiizzzz <ida.berg.home@gmail.com>
2025-02-26 15:57:40 +0100

    sort filen

--- correlation_code/sort_correlations.r
+++ correlation_code/sort_correlations.r
@@ -6,14 +6,19 @@
 
 # results <- "test_files/results_org_correlation_ww1_test.tsv"
 
-# correlation = "/storage/bergid/correlation/results_org_correlation_ww1_log.tsv"
+# correlation = "/storage/bergid/correlation/results_org_correlation_ww1_log.tsv" 
 # results = "/storage/bergid/correlation/results_org_correlation_ww2_log.tsv"
 # results = "/storage/bergid/correlation/results_org_correlation_hg_log.tsv"
 # results = "/storage/bergid/correlation/results_org_correlation_all_log.tsv"
-# /storage/bergid/correlation/results_org_correlation_all_log_switchLogSum.tsv
+
+# results = "/storage/bergid/correlation/results_org_correlation_ww1_log75.tsv" #correlation
+# results = "/storage/bergid/correlation/results_org_correlation_ww2_log75.tsv" #correlation_2
+# results = "/storage/bergid/correlation/results_org_correlation_hg_log75.tsv" #correlation_3
+# results = "/storage/bergid/correlation/results_org_correlation_all_log75.tsv"
+# results <- "/storage/bergid/correlation/results_org_correlation_all_log_sep75.tsv"
 
 
-df <- read.table("/storage/bergid/correlation/results_org_correlation_all_log_switchLogSum.tsv", 
+df <- read.table("/storage/bergid/correlation/results_org_correlation_hg_log75.tsv", 
                  sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 df_filtered <- subset(df, CorrelationCoefficient != 1 & !is.na(pValue) & pValue != 0)
@@ -22,7 +27,7 @@ df_filtered <- subset(df, CorrelationCoefficient != 1 & !is.na(pValue) & pValue 
 df_ordered <- df_filtered[order(-df_filtered$CorrelationCoefficient), ]  
 
 
-write.table(df_ordered, file = "/storage/bergid/correlation/results_org_correlation_all_log_ordered_switched.tsv", 
+write.table(df_ordered, file = "/storage/bergid/correlation/results_org_correlation_hg_log_ordered_75.tsv", 
             sep = "\t", quote = FALSE, row.names = FALSE)
 
 #/storage/bergid/correlation/results_org_correlation_ww1_log_ordered.tsv
