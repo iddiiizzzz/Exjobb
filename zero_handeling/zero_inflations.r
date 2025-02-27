7966654c284ccf7d9e45e76d700fcebd8de47775
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-02-27 14:19:17 +0100

    filtrering i zero_inflations

--- zero_handeling/zero_inflations.r
+++ zero_handeling/zero_inflations.r
@@ -4,23 +4,30 @@ library(pscl)
 library(MASS)
 library(reshape2)
 
-# "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv"
-# "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv"
-# "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv"
+# "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv" #ida
+# "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv" #ellen
+# "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv" #ellen
 # "/storage/bergid/taxonomy_rewrites/taxonomy_all_organisms.tsv"
 
 #test_files/rewritten_test_kraken1.tsv
 
 
 # Read your data
-data <- read.table("/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+data <- read.table("/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+#filter zeros
+data <- data[(rowSums(data == 0) / ncol(data)) < 0.75, ]
 
 
 # Set TrueID as row names and convert the data to long format
 data_long <- melt(data, id.vars = "TrueID", variable.name = "Sample", value.name = "Counts")
 
-data_long <- data_long[(rowSums(data_long == 0) / ncol(data_long)) < 0.75, ]
-data_long <- log(data_long + 1)
+
+# Convert TrueID to factor (for modeling)
+data_long$TrueID <- as.factor(data_long$TrueID)
+
+# Log-transform counts only
+data_long$Counts <- log(data_long$Counts + 1)
 
 # ZIP model
 zip_model <- zeroinfl(Counts ~ TrueID | 1, data = data_long, dist = "poisson")
