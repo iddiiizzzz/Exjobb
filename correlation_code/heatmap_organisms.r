25076b13b2bab49f87c7f59d6c8779e7dc7c2998
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-03-04 14:13:22 +0100

    heatmaps och omsortering av korrelationslistor

--- correlation_code/heatmap_organisms.r
+++ correlation_code/heatmap_organisms.r
@@ -9,18 +9,18 @@ library(pheatmap)
 library(gplots)
 library(RColorBrewer)
 
+file_path <- "/storage/bergid/correlation/results_org_correlation_all_log75.tsv"
+#file_path <- "/storage/bergid/correlation/results_org_correlation_hg_log75.tsv"
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww2_log75.tsv" #11
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww1_log75.tsv" #14
+#file_path <- "/storage/bergid/correlation/results_org_correlation_all_log_sep75.tsv" #15
+#file_path <- "/storage/bergid/correlation/results_org_correlation_hg_log.tsv" #16
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww2_log.tsv" #17
+#file_path <- "/storage/bergid/correlation/results_org_correlation_all_log.tsv" #18
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww1_log.tsv" #19
 
-# correlations <- read.table("test_files/results_org_correlation_ww1_test.tsv", 
-#                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-
-# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww1_log.tsv", 
-#                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-correlations <- read.table("/storage/bergid/correlation/results_org_correlation_ww2_log.tsv", 
-                           sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-# correlations <- read.table("/storage/bergid/correlation/results_org_correlation_hg_log.tsv", 
-#                            sep = "", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
-
-
+correlations <- read.table(file_path, sep = "\t", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
+ 
 # Convert long format to a wide format (matrix)
 cor_matrix <- dcast(correlations, Organism1 ~ Organism2, value.var = "CorrelationCoefficient")
 
@@ -34,14 +34,14 @@ cor_matrix[is.na(cor_matrix)] <- 0
 
 
 # Create a color palette using RColorBrewer
-my_palette <- colorRampPalette(brewer.pal(9, "YlOrRd"))(100)
+my_palette <- colorRampPalette(c("blue", "white", "red"))(100)
 
-pdf("correlation_code/heatmaps/heatmap_org_ww2.pdf")
-heatmap.2(cor_matrix, main = "WasteWater2 Correlation Heatmap (log-transformed)", 
-col=my_palette,
-trace = "none",
-margins = c(12,12))
+png("correlation_code/heatmaps/organisms/heatmap_org_all_log75.png", width = 30000, height = 30000, res = 4000)
+pheatmap(cor_matrix, 
+    col=my_palette,
+    main = "All Organisms Correlation Heatmap (log-transformed, filtered 75% zeros)", 
+    fontsize_row = 0.3,
+    fontsize_col = 0.3, 
+    angle_col = 90)
 dev.off()
-
-
 
--- correlation_code/sort_correlations.r
+++ correlation_code/sort_correlations.r
@@ -3,29 +3,35 @@
 # Order the rows in correlation results after coefficient value
 # ------------------------------------------------------------------
 
+# # file_path <- "test_files/results_org_correlation_ww1_test.tsv"
 
-# results <- "test_files/results_org_correlation_ww1_test.tsv"
-
-# correlation = "/storage/bergid/correlation/results_org_correlation_ww1_log.tsv" 
-# results = "/storage/bergid/correlation/results_org_correlation_ww2_log.tsv"
-# results = "/storage/bergid/correlation/results_org_correlation_hg_log.tsv"
-# results = "/storage/bergid/correlation/results_org_correlation_all_log.tsv"
-
-# results = "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww1_log75.tsv" 
-# results = "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww2_log75.tsv" 
-# results = "/storage/bergid/correlation/ordered_correlations/results_org_correlation_hg_log75.tsv" 
-# results = "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log75.tsv" 
-# results <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log_sep75.tsv" 
+file_path <- "/storage/bergid/correlation/results_org_correlation_all_log75.tsv" #correlation1 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_hg_log75.tsv" #correlation2 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww2_log75.tsv" #correlation3 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_hg_log.tsv" #correlation4 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww2_log.tsv" #correlation5 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_all_log.tsv" #correlation6 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww1_log75.tsv" #correlation7 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_all_log_sep75.tsv" #correlation8 (ellen)
+#file_path <- "/storage/bergid/correlation/results_org_correlation_ww1_log.tsv" #correlation9 (ellen)
 
 
-df <- read.table("/storage/bergid/correlation/results_org_correlation_all_log_sep75.tsv", 
-                 sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+### results  
+outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log_ordered_75.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_hg_log_ordered_75.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww2_log_ordered_75.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_hg_log_ordered.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww2_log_ordered.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log_ordered.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww1_log_ordered_75.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log_separate_ordered_75.tsv"
+#outfile <- "/storage/bergid/correlation/ordered_correlations/results_org_correlation_ww1_log_ordered.tsv"
 
-df_filtered <- subset(df, CorrelationCoefficient != 1 & !is.na(pValue) & pValue != 0)
+df <- read.table(file_path, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
-
+df_filtered <- subset(df, CorrelationCoefficient != 1)
 df_ordered <- df_filtered[order(-df_filtered$CorrelationCoefficient), ]  
 
+write.csv(df_ordered, outfile, row.names = FALSE)
 
-write.table(df_ordered, file = "/storage/bergid/correlation/ordered_correlations/results_org_correlation_all_log_separate_ordered_75.tsv", 
-            sep = "\t", quote = FALSE, row.names = FALSE)
+#excel funkar inte med så många rader
