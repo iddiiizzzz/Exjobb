ce7c93bfc85885b46be12f2a6c4ba8eb196b0cc6
Iddiiizzzz <ida.berg.home@gmail.com>
2025-03-04 14:46:56 +0100

    zip och zinb modeller

--- zero_handeling/zero_inflations.r
+++ zero_handeling/zero_inflations.r
@@ -4,35 +4,54 @@ library(pscl)
 library(MASS)
 library(reshape2)
 
-# "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv" #ida
-# "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv" #ellen
-# "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv" #ellen
-# "/storage/bergid/taxonomy_rewrites/taxonomy_all_organisms.tsv"
+# infile = "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv" 
+# infile = "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv" 
+# infile = "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv"
 
-#test_files/rewritten_test_kraken1.tsv
+infile = "test_files/rewritten_test_kraken1.tsv"
 
 
-# Read your data
-data <- read.table("/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+data <- read.table(infile, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
-#filter zeros
-data <- data[(rowSums(data == 0) / ncol(data)) < 0.75, ]
 
+# data <- data[(rowSums(data == 0) / ncol(data)) < 0.75, ]
+# Filter out rows where all counts are greater than zero
+data <- data[apply(data[, -1], 1, function(row) any(row == 0)), ]
+data$TrueID <- as.character(data$TrueID)
+
+zero_probabilities_zip <- matrix(0, nrow = nrow(data), ncol = ncol(data) - 1, dimnames = list(data$TrueID, colnames(data)[-1]))
+zero_probabilities_zinb <- matrix(0, nrow = nrow(data), ncol = ncol(data) - 1, dimnames = list(data$TrueID, colnames(data)[-1]))
 
 # Set TrueID as row names and convert the data to long format
 data_long <- melt(data, id.vars = "TrueID", variable.name = "Sample", value.name = "Counts")
 
+for (i in 1:nrow(data)) {
+    row_data <- data[i,-1] # Exclude the TrueID column
+    tax_id <- data[i,1] # TrueID for the row
 
-# Convert TrueID to factor (for modeling)
-data_long$TrueID <- as.factor(data_long$TrueID)
+    row_df <- data.frame(Sample = colnames(row_data), Counts = as.numeric(row_data))
+    
+    
+    # ZIP model
+    zip_model <- zeroinfl(Counts ~ 1 | 1, data = row_df, dist = "poisson")
 
-# Log-transform counts only
-data_long$Counts <- log(data_long$Counts + 1)
+    #ZINB model
+    zinb_model <- zeroinfl(Counts ~ 1 | 1, data = row_df, dist = "negbin")
 
-# ZIP model
-zip_model <- zeroinfl(Counts ~ TrueID | 1, data = data_long, dist = "poisson")
-summary(zip_model)
 
-# ZINB model
-zinb_model <- zeroinfl(Counts ~ TrueID | 1, data = data_long, dist = "negbin")
-summary(zinb_model)
+    # Predict zero-inflation probabilities for each column
+    zip_probs <- predict(zip_model, type = "zero")
+    zinb_probs <- predict(zinb_model, type = "zero")
+
+    # Store probabilities in the matrix
+    zero_probabilities_zip[tax_id, ] <- zip_probs
+    zero_probabilities_zinb[tax_id, ] <- zinb_probs
+}
+
+# Save the zero probabilities for further analysis
+write.table(zero_probabilities_zip, file = "test_files/zip_probabilities.tsv", sep = "\t", quote = FALSE, col.names = NA)
+write.table(zero_probabilities_zinb, file = "test_files/zinb_probabilities.tsv", sep = "\t", quote = FALSE, col.names = NA)
+ 
+print(zip_model)
+print(zinb_model)
+
