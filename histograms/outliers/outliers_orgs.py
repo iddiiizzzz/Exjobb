b5b1864c5bab59a6454d77adc9ce0c4f78d6f363
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-03-06 11:48:57 +0100

    organism outliers

--- histograms/outliers/outliers_orgs.py
+++ histograms/outliers/outliers_orgs.py
@@ -1,12 +1,12 @@
 
-# Organisms
-# count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv"
-# count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv"
-count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv"
+## Organisms
+count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_hg.tsv"
+#count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_ww1.tsv"
+#count_matrix = "/storage/bergid/taxonomy_rewrites/taxonomy_ww2.tsv"
 
-# outliers = "/storage/bergid/outliers/orgs_with_outliers_hg.tsv"
-# outliers = "/storage/bergid/outliers/orgs_with_outliers_ww1.tsv"
-# outliers = "/storage/bergid/outliers/orgs_with_outliers_ww2.tsv"
+outliers = "/storage/bergid/outliers/orgs_with_outliers_hg.tsv"
+#outliers = "/storage/bergid/outliers/orgs_with_outliers_ww1.tsv"
+#outliers = "/storage/bergid/outliers/orgs_with_outliers_ww2.tsv"
 
 
 # Set to track organisms that exceed the threshold
@@ -17,7 +17,7 @@ with open(count_matrix, "r") as infile:
     next(infile)  # Skip header
     
     for index, line in enumerate(infile):
-        print(f"Processing row {index}")
+        print(f"Processing organism {index}")
 
         columns = line.strip().split("\t") 
         identifier = columns[0]  # First column (organism ID)
