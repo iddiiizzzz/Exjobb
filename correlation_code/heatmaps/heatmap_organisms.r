69c3873b7bd5be5021a0e77f91eac1175eb6fb2a
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-04-04 11:56:58 +0200

    heatmaps rensat och testar storlek

--- correlation_code/heatmaps/heatmap_organisms.r
+++ correlation_code/heatmaps/heatmap_organisms.r
@@ -9,17 +9,15 @@ library(pheatmap)
 library(gplots)
 library(RColorBrewer)
 
-# file_path <- "/storage/bergid/correlation/organisms/org_correlation_zero_inflation_probabilities.tsv"  ##ellen tmux correlation2
+# file_path <- "/storage/bergid/correlation/organisms/org_correlation_zero_inflation_probabilities.tsv"
 # png <- "correlation_code/heatmaps/organisms/heatmap_orgs_probabilities.png" 
 
-# file_path <- "/storage/bergid/correlation/organisms/org_correlation_separate_filtering.tsv"  ##ellen tmux correlation
+# file_path <- "/storage/bergid/correlation/organisms/org_correlation_separate_filtering.tsv"
 # png <- "correlation_code/heatmaps/organisms/heatmap_orgs_filtered.png" 
 
-file_path <- "/storage/bergid/correlation/organisms/orgs_correlation_zero_inflation_threshold_06.tsv"  ##ellen tmux correlation3
-png <- "correlation_code/heatmaps/organisms/heatmap_orgs_threshold_0.6_resolution.png"
+file_path <- "/storage/bergid/correlation/organisms/orgs_correlation_zero_inflation_threshold_06.tsv"  ##ellen tmux correlation
+png <- "correlation_code/heatmaps/organisms/heatmap_orgs_threshold_0.6.png"
 
-# file_path <- "test_files/results_org_correlation_ww1_test.tsv"
-# png <- "test_files/heatmaps/heatmap_org_test.png"
 
 
 correlations <- read.table(file_path, sep = "\t", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE, fileEncoding = "UTF-8")
@@ -37,13 +35,13 @@ my_palette <- colorRampPalette(c("blue", "white", "red"))(100)
 breaks_list <- seq(-1, 1, length.out = 101)  # Ensures proper scaling from -1 to 1
 
 # 5000 width/height + res 900 bra för 181 st
-png(png, width = 32700, height = 32700, res = 10000)
+png(png, width = 30000, height = 30000, res = 3000)
 pheatmap(cor_matrix, 
     col = my_palette, 
     breaks = breaks_list,  # Fix scale between -1 and 1
     main = "Organisms Correlation (threshold 0.6, 90%)", 
-    fontsize_row = 0.06, 
-    fontsize_col = 0.06, 
+    fontsize_row = 0.16, 
+    fontsize_col = 0.16, 
     angle_col = 90)
 dev.off()
 
