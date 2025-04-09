2653b2be34c10421d17eb3bbd26586062608752a
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-04-09 13:16:33 +0200

    final heatmaps bot filtered & weighted

--- correlation_code/heatmaps/heatmap_both.r
+++ correlation_code/heatmaps/heatmap_both.r
@@ -10,8 +10,8 @@ library(gplots)
 library(RColorBrewer)
 
 
-file_path <- "/storage/bergid/correlation/both/final_correlation_weighted.tsv"
-png <- "correlation_code/heatmaps/both/heatmap_both_weighted.png"
+file_path <- "/storage/bergid/correlation/both/final_correlation_filtered.tsv"
+png <- "correlation_code/heatmaps/both/heatmap_both_filtered.png"
 
 
 correlations <- read.table(file_path, sep = "\t", header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE)
@@ -26,14 +26,14 @@ cor_matrix[is.na(cor_matrix)] <- 0
 
 
 my_palette <- colorRampPalette(c("blue", "white", "red"))(100)
-breaks_list <- seq(-0.1, 0.1, length.out = 101)  # Ensures proper scaling from -1 to 1
+breaks_list <- seq(-0.3, 0.3, length.out = 101)  # Ensures proper scaling from -1 to 1
 
 # 5000 width/height + res 900 bra för 181 st
 png(png, width = 6000, height = 6000, res = 900)
 pheatmap(cor_matrix, 
     col = my_palette, 
     breaks = breaks_list,  # Fix scale between -1 and 1
-    main = "ARG-Host Correlation Heatmap (weighted)", 
+    main = "ARG-Host Correlation Heatmap (filtered)", 
     fontsize_row = 1, 
     fontsize_col = 1, 
     angle_col = 90)
