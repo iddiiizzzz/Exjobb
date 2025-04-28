2b992f9d6be10a09ac695329649614818b0cb1ca
Iddiiizzzz <ida.berg.home@gmail.com>
2025-04-28 09:08:15 +0200

    scatterplots

--- checking_data/scatterplot_correlation.r
+++ checking_data/scatterplot_correlation.r
@@ -3,13 +3,22 @@
 library(ggplot2)
 library(reshape2)
 
-count_data_genes <- read.table("/storage/koningen/genus/combined_matrices/taxonomy_all_genes.tsv", sep = "\t", header = TRUE, row.names = 1)
-count_data_orgs <- read.table("/storage/koningen/genus/combined_matrices/taxonomy_all_organisms.tsv", sep = "\t", header = TRUE, row.names = 1)
-output_file <- "checking_data/test.png"
+# count_matrix_genes <- "/storage/koningen/genus/combined_matrices/taxonomy_all_genes.tsv"
+# count_matrix_orgs <- "/storage/koningen/genus/combined_matrices/taxonomy_all_organisms.tsv"
+count_matrix_genes <- "/storage/koningen/species/combined_matrices/taxonomy_all_genes.tsv"
+count_matrix_orgs <- "/storage/koningen/species/combined_matrices/taxonomy_all_organisms.tsv"
+
+output_file <- "checking_data/correlation_plots/scatterplot_corr_species_2.png"
 
 
-gene <- "GCA_025660525.1_ASM2566052v1_JAOSFK010000051.1_seq1...class_D_2"
-organism <- "Lysobacter"
+gene <- "GCA_001572875.1_ASM157287v1_LRUV01000056.1_seq1...class_D_2"
+organism <- "Serratia rhizosphaerae"
+
+
+
+count_data_genes <- read.table(count_matrix_genes, sep = "\t", header = TRUE, row.names = 1)
+count_data_orgs <- read.table(count_matrix_orgs, sep = "\t", header = TRUE, row.names = 1)
+
 
 gene_counts <- count_data_genes[gene, ]
 org_counts <- count_data_orgs[organism, ]
@@ -35,4 +44,4 @@ ggplot(combined_df, aes(x = Sample, y = Count, color = Type, shape = Type)) +
   scale_shape_manual(values = c("Gene" = 20, "Organism" = 16)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 0.01))
 
-ggsave(output_file, bg = "white", width = 12, height = 5, dpi = 900)
+ggsave(output_file, bg = "white", width = 12, height = 8, dpi = 900)
