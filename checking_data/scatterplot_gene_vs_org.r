c0728c97ae04dd1e84f801701030092947036a67
Iddiiizzzz <ida.berg.home@gmail.com>
2025-05-05 11:06:54 +0200

    scatterplots gjorda

--- checking_data/scatterplot_gene_vs_org.r
+++ checking_data/scatterplot_gene_vs_org.r
@@ -20,18 +20,38 @@
 library(ggplot2)
 library(reshape2)
 
-count_matrix_genes <- "/storage/koningen/genus/normalize/normalized_count_matrix_all_genes.tsv"
-count_matrix_orgs <- "/storage/koningen/genus/normalize/normalized_count_matrix_all_orgs.tsv"
-output_file <- "checking_data/scatterplot_gene_vs_org_genus.png"
+# Genus
+# count_matrix_genes <- "/storage/koningen/genus/normalize/normalized_count_matrix_all_genes.tsv"
+# count_matrix_orgs <- "/storage/koningen/genus/normalize/normalized_count_matrix_all_orgs.tsv"
+# output_file <- "checking_data/scatterplot_gene_vs_org_genus_low.png"
+
+# count_matrix_genes <- "/storage/koningen/genus/normalize/normalized_count_matrix_hg_genes.tsv"
+# count_matrix_orgs <- "/storage/koningen/genus/normalize/normalized_count_matrix_hg.tsv"
+# output_file <- "checking_data/scatterplot_gene_vs_org_genus_hg.png"
+
+# count_matrix_genes <- "/storage/koningen/genus/normalize/normalized_count_matrix_ww_genes.tsv"
+# count_matrix_orgs <- "/storage/koningen/genus/normalize/normalized_count_matrix_ww.tsv"
+# output_file <- "checking_data/scatterplot_gene_vs_org_genus_ww.png"
 
 
+# Species
 # count_matrix_genes <- "/storage/koningen/species/normalize/normalized_count_matrix_all_genes.tsv"
 # count_matrix_orgs <- "/storage/koningen/species/normalize/normalized_count_matrix_all_orgs.tsv"
 # output_file <- "checking_data/scatterplot_gene_vs_org_species.png"
 
+# count_matrix_genes <- "/storage/koningen/species/normalize/normalized_count_matrix_hg_genes.tsv"
+# count_matrix_orgs <- "/storage/koningen/species/normalize/normalized_count_matrix_hg.tsv"
+# output_file <- "checking_data/scatterplot_gene_vs_org_species_hg.png"
 
-gene <- "GCA_000203195.1_ASM20319v1_FR824044.1_seq1...tet_rpg"
-organism <- "Eubacterium"
+count_matrix_genes <- "/storage/koningen/species/normalize/normalized_count_matrix_ww_genes.tsv"
+count_matrix_orgs <- "/storage/koningen/species/normalize/normalized_count_matrix_ww.tsv"
+output_file <- "checking_data/scatterplot_gene_vs_org_species_ww.png"
+
+
+
+
+gene <- "GCA_945987735.1_ERR2020023_bin.17_metaWRAP_v1.3_MAG_CAMFTF010000038.1_seq1...erm_typeF"
+organism <- "Aerococcus urinaeequi"
 
 
 
@@ -53,7 +73,7 @@ ggplot(df, aes(x = GeneCounts, y = OrgCounts)) +
   labs(title = paste(gene, "and", organism),
        x = "Gene Count Value",
        y = "Organism Count Value") + 
-  theme_bw() + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 0.01))
+  theme_bw() +
+  ylim(0, 0.04)
 
 ggsave(output_file, bg = "white", width = 12, height = 8, dpi = 900)
