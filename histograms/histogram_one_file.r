647ba0b7c5c36902b5d46ad7cca67f8f198ac3a3
Ellen Koningen <koningen@saga.math.chalmers.se>
2025-05-20 16:19:58 +0200

    nya histogram

--- histograms/histogram_one_file.r
+++ histograms/histogram_one_file.r
@@ -28,7 +28,7 @@ showtext_auto()
 ##all genes + gene highest mean
 
 # Gene counts
-# infile = "/storage/koningen/count_matrix.tsv"
+infile = "/storage/koningen/count_matrix.tsv"
 
 # infile = "/storage/koningen/ranked_counts/individual_counts/highest_individual_counts_gene.tsv"
 # infile = "/storage/koningen/ranked_counts/individual_counts/lowest_individual_counts_gene.tsv"
@@ -36,12 +36,13 @@ showtext_auto()
 # infile = "/storage/koningen/ranked_counts/sum_counts/highest_sum_counts_gene.tsv"
 # infile = "/storage/koningen/ranked_counts/sum_counts/lowest_sum_counts_gene.tsv"
 
-infile = "/storage/koningen/ranked_counts/average_counts/highest_average_counts_gene.tsv"
+# infile = "/storage/koningen/ranked_counts/average_counts/highest_average_counts_gene.tsv"
 # infile = "/storage/koningen/ranked_counts/average_counts/lowest_average_counts_gene.tsv"
 
-
-
 # outfile = "histograms/bilder/genes/genes_all/histogram_genes.eps"
+# outfile = "histograms/bilder/genes/genes_all/log_transformed_histogram_genes.eps"
+outfile = "histograms/bilder/genes/genes_all/sqrt_transformed_histogram_genes.eps"
+
 
 # outfile = "histograms/bilder/genes/one_gene_histograms/highest_individual_count_gene_log.eps"
 # outfile = "histograms/bilder/genes/one_gene_histograms/lowest_individual_count_gene_sqrt.eps"
@@ -49,7 +50,10 @@ infile = "/storage/koningen/ranked_counts/average_counts/highest_average_counts_
 # outfile = "histograms/bilder/genes/one_gene_histograms/highest_sum_count_gene_sqrt.eps"
 # outfile = "histograms/bilder/genes/one_gene_histograms/lowest_sum_count_gene_sqrt.eps"
 
-outfile = "histograms/bilder/genes/one_gene_histograms/highest_mean_count_gene_sqrt.eps"
+# outfile = "histograms/bilder/genes/one_gene_histograms/highest_mean_count_gene.eps"
+# outfile = "histograms/bilder/genes/one_gene_histograms/highest_mean_count_gene_log.eps"
+
+# outfile = "histograms/bilder/genes/one_gene_histograms/highest_mean_count_gene_sqrt.eps"
 # outfile = "histograms/bilder/genes/one_gene_histograms/lowest_mean_count_gene_sqrt.eps"
 
 
@@ -63,27 +67,25 @@ data <- read.table(infile, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 values <- as.numeric(unlist(data[, -1]))  
 
 
-values <- log(values+1) 
-# values <- sqrt(values)
+# values <- log(values+1) 
+values <- sqrt(values)
 df <- data.frame(values = values)
 
 
 # change binwidth depending on highest count. 1000 for 44 000 000. 1 for 1000
 ggplot(df, aes(x = values)) +
-  geom_histogram(binwidth = 1, fill = "#00cfba", boundary = 0.5) +
-  labs(title = "Log-transformed histogram of gene with highest individual count", 
-       x = "Count Value", 
+  geom_histogram(binwidth = 1, fill = "#9b13bb", boundary = 0.5) +
+  labs(x = "Count Value", 
        y = "Number of counts") +
   theme_bw() +
   theme(
     text = element_text(family = "LM Roman"),         # Set global font
-    plot.title = element_text(size = 40),         # Title font size
-    axis.title.x = element_text(size = 40),       # X-axis title font size
-    axis.title.y = element_text(size = 40),       # Y-axis title font size
-    axis.text.x = element_text(size = 40),        # X-axis tick label size
-    axis.text.y = element_text(size = 40)         # Y-axis tick label size
-  )
- # ylim(0, 200000)
+    axis.title.x = element_text(size = 20),       # X-axis title font size
+    axis.title.y = element_text(size = 20),       # Y-axis title font size
+    axis.text.x = element_text(size = 15),        # X-axis tick label size
+    axis.text.y = element_text(size = 15)         # Y-axis tick label size
+  ) +
+  ylim(0, 500)
 
 ggsave(outfile, bg = "white", device = "eps")
  
